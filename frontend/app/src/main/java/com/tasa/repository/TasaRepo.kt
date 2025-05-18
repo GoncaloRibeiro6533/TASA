71c6e7a9d7568b79266d84e8949a45fea34113b0
GoncaloRibeiro6533 <goncaloribeiro6533@gmail.com>
2025-05-18 19:30:09 +0100

    fix

--- frontend/app/src/main/java/com/tasa/repository/TasaRepo.kt
+++ frontend/app/src/main/java/com/tasa/repository/TasaRepo.kt
@@ -1,5 +1,6 @@
 package com.tasa.repository
 
+import com.tasa.repository.interfaces.AlarmRepository
 import com.tasa.repository.interfaces.TasaRepoInterface
 import com.tasa.service.TasaService
 import com.tasa.storage.TasaDB
@@ -9,4 +10,7 @@ class TasaRepo(
     remote: TasaService,
 ) : TasaRepoInterface {
     override val userRepo = UserRepository(local, remote)
+    override val locationRepo = LocationRepository(local, remote)
+    override val eventRepo = EventRepository(local, remote)
+    override val alarmRepo = AlarmRepository(local)
 }
