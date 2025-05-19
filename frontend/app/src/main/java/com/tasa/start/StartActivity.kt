d1490fae17832d67ab7a39c5fbe1307db771d12f
GoncaloRibeiro6533 <goncaloribeiro6533@gmail.com>
2025-05-19 20:02:01 +0100

    fixed Start screen

--- frontend/app/src/main/java/com/tasa/start/StartActivity.kt
+++ frontend/app/src/main/java/com/tasa/start/StartActivity.kt
@@ -3,20 +3,36 @@ package com.tasa.start
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import com.tasa.DependenciesContainer
 import com.tasa.authentication.login.LoginActivity
 import com.tasa.authentication.register.RegisterActivity
+import com.tasa.homepage.HomePageActivity
 import com.tasa.ui.theme.TasaTheme
 import com.tasa.utils.navigateTo
 
 class StartActivity : ComponentActivity() {
+    private val userInfoRepository by lazy { (application as DependenciesContainer).userInfoRepository }
+
+    private val viewModel by viewModels<StartScreenViewModel>(
+        factoryProducer = {
+            StartScreenViewModelFactory(userInfoRepository)
+        },
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
+        viewModel.getSession()
         setContent {
             TasaTheme {
                 StartScreen(
+                    viewModel = viewModel,
                     onLoginRequested = { navigateTo(this, LoginActivity::class.java) },
                     onRegisterRequested = { navigateTo(this, RegisterActivity::class.java) },
+                    onAboutRequested = { /* TODO: Navigate to About screen */ },
+                    onLoggedIntent = { navigateTo(this, HomePageActivity::class.java) },
                 )
             }
         }
--- frontend/app/src/main/java/com/tasa/start/StartScreen.kt
+++ frontend/app/src/main/java/com/tasa/start/StartScreen.kt
@@ -5,17 +5,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.tasa.ui.components.NavigationHandlers
+import com.tasa.ui.components.TopBar
 import com.tasa.ui.theme.TasaTheme
 
 @Composable
 fun StartScreen(
-    onLoginRequested: () -> Unit,
-    onRegisterRequested: () -> Unit,
+    viewModel: StartScreenViewModel,
+    onAboutRequested: () -> Unit,
+    onLoginRequested: () -> Unit = { },
+    onRegisterRequested: () -> Unit = { },
+    onLoggedIntent: () -> Unit = { },
 ) {
     TasaTheme {
         Scaffold(
@@ -23,8 +28,14 @@ fun StartScreen(
                 Modifier
                     .fillMaxSize()
                     .background(MaterialTheme.colorScheme.background),
+            topBar = {
+                TopBar(
+                    NavigationHandlers(
+                        onAboutRequested = onAboutRequested,
+                    ),
+                )
+            },
         ) { innerPadding ->
-
             Column(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -33,10 +44,20 @@ fun StartScreen(
                         .fillMaxSize()
                         .padding(innerPadding),
             ) {
-                StartView(
-                    onLoginRequested = onLoginRequested,
-                    onRegisterRequested = onRegisterRequested,
-                )
+                when (viewModel.state) {
+                    StartScreenState.Idle -> {
+                        HomePopPup()
+                    }
+                    StartScreenState.Logged -> {
+                        onLoggedIntent()
+                    }
+                    StartScreenState.NotLogged -> {
+                        StartView(
+                            onLoginRequested = onLoginRequested,
+                            onRegisterRequested = onRegisterRequested,
+                        )
+                    }
+                }
             }
         }
     }
